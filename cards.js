c94d98683013e57b63c61106c0e523c5e55cdd1a
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-21 16:16:22 -0600

    created create cards function/elements/attributes

--- cards.js
+++ cards.js
@@ -1,4 +1,35 @@
 let createButton = document.getElementById("create");
 let textArea = document.getElementById("textArea");
+let newTextArea = document.getElementById("newTextArea");
 
-createButton.addEventListener("click")
+// write an EL to target the create new card button
+createButton.addEventListener("click", () => {
+    createCard();
+    textArea.value = "";
+});
+
+function createCard() {
+    // create the elments
+    let input = textArea.value;
+    let newCard = document.createElement("div");
+    let newCardText = document.createElement("p");
+    let deleteButton = document.createElement("button");
+
+    // create the attributes 
+    newCard.classList.add("newCard");
+    deleteButton.id = "delte-button";
+    newCardText.classList.add("newCardText");
+    deleteButton.innerHTML = "delete";
+
+    // add to DOM
+    newCard.appendChild(newCard);
+    deleteButton.appendChild(deleteButton);
+    newCardText.appendChild(newCardText);
+    newCard.appendChild(deleteButton);
+}
+
+// create function to delete new card
+
+function deleteNewCard(card) {
+    newTextArea.removeChild(card);
+};
